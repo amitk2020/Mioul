076ba5df82a2dd780770ab79ee1a14ce29349e87
Amit <amit.k03377@gmail.com>
2021-10-23 20:54:27 +1300

    menu slidr functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
-
+    var menuCancel = $('.slide-menu-header');
     $('.menu').click('on', mennSliderVisible);
     var menuSlider = $('.slide-menu-container');
     function mennSliderVisible(){
         menuSlider.addClass('visible');
         // $("body").addClass('overlay');
     }
+    menuCancel.on('click',function(){
+        menuSlider.removeClass('visible');
+    });
+
   
 });
